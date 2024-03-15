f7d0b4a062e60c37902efd30d2db2ddf9b3737d0
Carl Alquisola <carlanthony.alquisola@va.gov>
2024-03-15 17:11:53 -0400

    test filters

--- .github/actions/get-image-details/get-details.js
+++ .github/actions/get-image-details/get-details.js
@@ -7,47 +7,40 @@ const getImageDetails = () => {
     const { owner, repo } = github.context.repo;
     const tag = core.getInput('tag').replace(/\//g, '-');
     const imageNameFilter = core.getInput('name');
-    const foundImage = images.find(
-      img => img['service-name'] === imageNameFilter,
-    );
+    const foundImage = images.find(img => img['service-name'] === imageNameFilter);
 
     if (!foundImage) {
       throw new Error(`${imageNameFilter} is an invalid image name`);
     }
 
-    // get the base node image
-    const baseImage = images.find(
-      img => img['is-base-image']
-    )
-
+    const baseImage = images.find(img => img['is-base-image']);
     const baseNodeImage = `ghcr.io/${owner}/${repo}/${baseImage['service-name']}:${tag}`;
 
     const imageName = foundImage['service-name'];
     const context = foundImage['context'] ?? '.';
     const dockerfilePath = `${context}/Dockerfile${foundImage['named-dockerfile'] ? '.' + imageName : ''}`;
-    // Store file path dependencies as a comma-separated string
     const dependencies = [
       dockerfilePath,
       ...(foundImage['path-dependencies'] ?? []),
     ].join();
     const ghcrName = `ghcr.io/${owner}/${repo}/${imageName}`;
 
-    // remove is-base-image property in the result
-    const { 'is-base-image': _, ...restFoundImage } = foundImage;
-
-    const matrix = {
-      ...restFoundImage,
+    // Prepare the output data by excluding 'is-base-image' property and adding new fields
+    const outputData = {
+      ...foundImage,
       context,
       'docker-file-path': dockerfilePath,
       'tagged-ghcr-name': `${ghcrName}:${tag}`,
       'ghcr-name': ghcrName,
       'path-dependencies': dependencies,
-      'base-image':
-        (['backend', 'frontend'].includes(imageName) && baseNodeImage) || '',
+      'base-image': (['backend', 'frontend'].includes(imageName) && baseNodeImage) || '',
     };
 
-    core.setOutput('matrix', JSON.stringify(matrix));
-    console.log('Output:', JSON.stringify(matrix, null, 2));
+    // Exclude 'is-base-image' property from the output data
+    delete outputData['is-base-image'];
+
+    core.setOutput('image-details', JSON.stringify(outputData));
+    console.log('Output:', JSON.stringify(outputData, null, 2));
   } catch (error) {
     core.setFailed(error.message);
     console.error(error);
