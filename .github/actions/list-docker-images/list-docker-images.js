24b6dad78c390cd442cb5df187086daa8298fea7
Carl Alquisola <carlanthony.alquisola@va.gov>
2024-03-15 12:39:13 -0400

    test filters

--- .github/actions/list-docker-images/list-docker-images.js
+++ .github/actions/list-docker-images/list-docker-images.js
@@ -7,7 +7,9 @@ const listDockerImages = () => {
     const { owner, repo } = github.context.repo;
     const tag = core.getInput('tag').replace(/\//g, '-');
     const includeBaseImage = core.getInput('include-base-image').replace(/\//g, '-');
+    console.log(`includeBaseImage: ${includeBaseImage}`);
     console.log(`includeBaseImage: ${typeof(includeBaseImage)}`);
+
     let filteredImages = images;
 
     // filter out the node base image, includeBaseImage is a string
